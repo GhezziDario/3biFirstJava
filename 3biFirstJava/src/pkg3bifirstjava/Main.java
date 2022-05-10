6d592ed261719c86c50bfcc5c1a24f4532339c26
Ghezzi Dario <ghezzi.dario@osdbsesto>
2022-05-10 13:33:23 +0200

    add loop

--- 3biFirstJava/src/pkg3bifirstjava/Main.java
+++ 3biFirstJava/src/pkg3bifirstjava/Main.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg3bifirstjava;
-
-/**
- *
- * @author ghezzi.dario
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+     for (int x = 0;x<5;x++)
+     {
+         System.out.println(x);
+     }
     }
     
 }
